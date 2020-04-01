c78d22b4122a2722485140000887b78a120c5e08
aman-atg <amanansari.a2@gmail.com>
2020-04-02 00:27:56 +0530

    validating email

---
+++ index.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const emailValue = email.value;
+  console.log(emailValue);
+  if (validateEmail(emailValue)) form.classList.remove("error");
+  else form.classList.add("error");
+});
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
